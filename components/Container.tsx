690a29d70cce84d2c872aa9c69f650496b6d2eb8
Nada Farook <nadaaofficial19@gmail.com>
2022-08-21 00:20:13 +0530

    Analytics

--- components/Container.tsx
+++ components/Container.tsx
@@ -61,7 +61,10 @@ export default function Container(props) {
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )}
+
+<script async src="https://cdn.splitbee.io/sb.js"></script>
       </Head>
+
       <div className="flex flex-col justify-center px-8">
         <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
